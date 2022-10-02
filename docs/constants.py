dbed1bd134d19e6fa0b646086e0c24d9b0dac835
Guille21497 <32059549+Guille21497@users.noreply.github.com>
2022-10-02 23:55:11 +0200

    Update constants.py

--- docs/constants.py
+++ docs/constants.py
@@ -1,3 +1,3 @@
-TOKEN_TG =  "5770033505:AAH3DVUupmvneAzrfOXGjd1S8lhQFFOPj60" 
-BITLY_KEY = "fde5d71c4f28a24ad80f463e45156bc8a63f87ae"
+TOKEN_TG =  "5770xxx" 
+BITLY_KEY = "fdxxxx"
 OPENWEATHERMAP_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
