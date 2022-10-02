6f970bffcf9a94f9083828cd326602496e6b1206
Guille21497 <32059549+Guille21497@users.noreply.github.com>
2022-10-02 15:35:12 +0200

    Update tokens

--- docs/constants.py
+++ docs/constants.py
@@ -1,3 +1,3 @@
-TOKEN_TG =  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" 
-BITLY_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+TOKEN_TG =  "5770033505:AAH3DVUupmvneAzrfOXGjd1S8lhQFFOPj60" 
+BITLY_KEY = "fde5d71c4f28a24ad80f463e45156bc8a63f87ae"
 OPENWEATHERMAP_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
